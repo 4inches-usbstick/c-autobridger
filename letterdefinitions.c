a57eaddf1d5f4127b477b6bbf32787b4c19fa28d
Charles <75965179+4inches-usbstick@users.noreply.github.com>
2021-10-09 21:35:34 -0400

    Create letterdefinitions.c

---
+++ letterdefinitions.c
@@ -0,0 +1,16 @@
+#define A 65
+#define B 66
+#define C 67
+#define D 68
+#define E 69
+#define F 70
+#define G 71
+#define H 72
+#define I 73
+#define J 74
+#define K 75
+#define L 76
+#define M 77
+#define N 78
+#define O 79
+#define P 80
